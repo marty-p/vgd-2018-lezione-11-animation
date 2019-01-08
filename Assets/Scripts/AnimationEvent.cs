249000e240e9097861bd7105c3f0c98a1ee12d1c
marty-p <37085112+marty-p@users.noreply.github.com>
2019-01-08 15:57:18 +0000

    Add files via upload

---
+++ Assets/Scripts/AnimationEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvent : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            animator.SetBool("rotation", true);
+        }
+
+    }
+
+    public void Event()
+    {
+        animator.SetBool("bounce", true);
+        Debug.Log("Questo è un evento");
+    }
+}
