677407d27ba6264f70f96e165f449588ad30b5dc
marty-p <37085112+marty-p@users.noreply.github.com>
2019-01-14 15:16:09 +0100

    Update MoveTo.cs

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -3,9 +3,11 @@ using UnityEngine.AI;
 public class MoveTo : MonoBehaviour {
     public Transform goal;
     private NavMeshAgent agent;
+    private Animator anim;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
     }
     private void Update()
     {
