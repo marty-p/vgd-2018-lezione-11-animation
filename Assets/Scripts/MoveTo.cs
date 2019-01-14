60b4646efdf59984344077358f09faef6574a4dc
marty-p <37085112+marty-p@users.noreply.github.com>
2019-01-14 15:27:49 +0100

    Update MoveTo.cs

--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -11,10 +11,16 @@ public class MoveTo : MonoBehaviour {
     }
     private void Update()
     {
+        // magnitude goes between 0 and speed
+        // speed is the max velocity
+        // velocity is a vector3d of how fast the agent is currently going
         agent.destination = goal.position;
+        // check if agent is moving
         if (agent.velocity.magnitude > 0)
             anim.SetBool("isMoving", true);
         else
             anim.SetBool("isMoving", false);
+        // set velocity
+        anim.SetFloat("Velocity", agent.velocity.magnitude / agent.speed);
     }
 }
